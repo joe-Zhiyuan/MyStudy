e1a991750bb45a57676a4577ee41e06dd04a1a5c
joe-Zhiyuan <849975459@qq.com>
2018-04-26 16:51:46 +0800

    高程之workers

--- javascript/js/alert1.js
+++ javascript/js/alert1.js
@@ -1,3 +1,11 @@
-function alert1(){
-    alert("alert.js");
+function timedCount(){
+    for(var j = 0; j < 100; j++){
+        var i = j;
+    }
+    postMessage(i);
 }
+
+postMessage("开始执行时间：" + new Date);
+timedCount();
+postMessage("结束执行时间：" + new Date);
+
