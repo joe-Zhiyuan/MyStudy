584c645f39c3f26063e5a787f7f5b1017e9663bf
joe-Zhiyuan <849975459@qq.com>
2018-05-07 17:23:30 +0800

    学习
    学习高程3-ECMAScript Harmony

--- javascript/js/alert1.js
+++ javascript/js/alert1.js
@@ -1,11 +1,19 @@
-function timedCount(){
-    for(var j = 0; j < 100; j++){
-        var i = j;
-    }
-    postMessage(i);
-}
-
-postMessage("开始执行时间：" + new Date);
-timedCount();
-postMessage("结束执行时间：" + new Date);
-
+// function timedCount(){
+//     for(var j = 0; j < 100; j++){
+//         var i = j;
+//     }
+//     postMessage(i);
+// }
+//
+// postMessage("开始执行时间：" + new Date);
+// timedCount();
+// postMessage("结束执行时间：" + new Date);
+self.onmessage = function(event){
+    var data = event.data;
+    data.sort(function(a, b){
+        return a - b;
+    });
+    self.postMessage(data);
+};
+// 停止工作
+// self.close();
