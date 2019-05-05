6a8c36d958daed61e1555b319a03556a6bd11489
joe-Zhiyuan <849975459@qq.com>
2019-05-06 03:26:13 +0800

    webpack+vue
    打包配置文件+插件+模块

--- nodeDemo/webpack.config.js
+++ nodeDemo/webpack.config.js
@@ -49,7 +49,27 @@ const config = {
             }
         }),
         new HTMLPlugin()
-    ]
+    ],
+    //webpack4 打包代码
+    optimization: {
+        splitChunks:{
+            cacheGroups: {
+                commons:{
+                    chunks: 'initial',
+                    minChunks: 2,
+                    maxInitialRequests: 5,
+                    minSize: 0
+                },
+                vendor: {
+                    test: /node_modules/,
+                    chunks: 'initial',
+                    name: 'vendor',
+                    priority: 10,
+                    enforce: true
+                }
+            }
+        }
+    }
 };
 //获取到运行标识
 if(isDev){
@@ -83,6 +103,13 @@ if(isDev){
         new webpack.NoEmitOnErrorsPlugin()
     )
 }else{
+    // 打包配置文件 插件
+    config.entry = {
+        app: path.join(__dirname, "src/index.js"),
+        vendor: ['vue']
+    }
+    //webpack4
+
     config.output.filename = '[name].[chunkhash:8].js';
     config.module.rules.push(
         {
@@ -105,7 +132,17 @@ if(isDev){
     config.plugins.push(
         //打包出错 不适配webpack4 安装extract-text-webpack-plugin@next contenthash改为chunkhash
         // new ExtractPlugin('styles.[contenthash  :8].css')
-        new ExtractPlugin('styles.[chunkhash:8].css')
+        new ExtractPlugin('styles.[chunkhash:8].css'),
+        // //放在下一个前面 webpack4.0废除 在上边↑
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'vendor'
+        // }),
+        // // webpack单独打包 避免打包后加入新模块id导致的变化
+        // //理论上只要文件内容没变，chunkhash 就不会变。但是 webpack 会在打包完的 bundle 中插入 runtime 代码
+        // //即使文件内容没变，但 runtime 可能会变，导致 chunkhash 改变，所以才要把 runtime 单独拆分出来
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'runtime'
+        // })
     )
 }
 
